7b20ef59adbcbb89e25cba5c017e0a64c3912643
Ryan <rageorge@ucsd.edu>
2014-04-08 16:53:47 -0700

    Added in a Collada parser (GrendgineCollada). Documented Vertex4 and Matrix4 classes so their functionality is more clear. Added in various base classes for model and mesh rendering (currently not finished, but will compile).

---
+++ Breakneck Brigade/Breakneck Brigade/Graphics/Vertex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SousChef;
+
+namespace Breakneck_Brigade.Graphics
+{
+    /*
+     * Holds data about a vertex, namely the location in model space and its normal;
+     */
+    class Vertex
+    {
+        public Vector4 Position;
+        public Vector4 Normal;
+        public Vector4 TextureCoordinates;
+
+        public Vertex(Vector4 pos, Vector4 norm, Vector4 tc)
+        {
+            this.Position           = pos;
+            this.Normal             = norm;
+            this.TextureCoordinates = tc;
+        }
+
+        public Vertex(float posX, float posY, float posZ, float normX, float normY, float normZ, float tcX, float tcY, float tcZ)
+        {
+            this.Position               = new Vector4(posX, posY, posZ);
+            this.Normal                 = new Vector4(normX, normY, normZ);
+            this.TextureCoordinates     = new Vector4(tcX, tcY, tcZ);
+        }
+        public Vertex(double posX, double posY, double posZ, double normX, double normY, double normZ, double tcX, double tcY, double tcZ)
+        {
+            this.Position               = new Vector4(posX, posY, posZ);
+            this.Normal                 = new Vector4(normX, normY, normZ);
+            this.TextureCoordinates     = new Vector4(tcX, tcY, tcZ);
+        }
+    }
+}
--- Breakneck Brigade/SousChef/Vector4.cs
+++ Breakneck Brigade/SousChef/Vector4.cs
@@ -1,24 +1,23 @@
-/*
- * SousChef Vector4 Class
- * An implementation of a 4D vector for use in homogenous matrix operations
- * 
- * Important Note: The vast majority of the operations do not do true 4D 
- * vector math, and omit the 4th coordinate. For example, dotting two Vector4s 
- * will not add the final 1*1 coordinate.
- * 
- * Adapted from code by Jurgen Schulze and Steve Rotenburg
- * Written by Ryan George
- */
 using System;
 namespace SousChef
 {
+    /// <summary>
+    /// An implementation of a 4D vector for use in homogenous matrix operations
+    /// 
+    /// Important Note: The vast majority of the operations do not do true 4D 
+    /// vector math, and omit the 4th coordinate. For example, dotting two Vector4s 
+    /// will not add the final 1*1 coordinate.
+    /// </summary>
+    /// <author>
+    /// Ryan George
+    /// </author>
     public class Vector4
     {
         float[] v;              //Backing array for the object
 
-        /*
-         * No arg constructor that initializes all dimensions to 0
-         */
+       /// <summary>
+       /// Initializes all dimensions of the vector to 0.
+       /// </summary>
         public Vector4()
         {
             v = new float[4];
@@ -28,9 +27,10 @@ namespace SousChef
 	        }
         } 
 
-        /*
-         * Copy constructor
-         */
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        /// <param name="other"></param>
         public Vector4(Vector4 other)
         {
             v = new float[4];
@@ -40,9 +40,13 @@ namespace SousChef
             v[3] = other[3];
         }
 
-        /*
-         * Constructor that initializes all dimensions to their specified values
-         */
+        /// <summary>
+        /// Sets the dimension of the vector to the specified values
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="w"></param>
         public Vector4(float x, float y, float z, float w)
         {
 	        v[0] = x;
@@ -51,9 +55,13 @@ namespace SousChef
 	        v[3] = w;
         }
 
-        /*
-         * Constructor that initializes all dimensions to their specified values
-         */
+        /// <summary>
+        /// Sets the dimension of the vector to the specified values
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="w"></param>
         public Vector4(double x, double y, double z, double w)
         {
 	        v[0] = (float) x;
@@ -62,10 +70,13 @@ namespace SousChef
 	        v[3] = (float) w;
         }
 
-        /*
-         * Constructor that initializes only x, y, and z dimensions.
-         * Initializes homogenous coordinate to 1, indicating a 3D vector
-         */
+        /// <summary>
+        /// Constructor that initializes only x, y, and z dimensions.
+        /// Initializes homogenous coordinate to 1, indicating a 3D vector
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
         public Vector4(float x, float y, float z)
         {
             v[0] = x;
@@ -74,10 +85,13 @@ namespace SousChef
             v[3] = 1;
         }
 
-        /*
-         * Constructor that initializes only x, y, and z dimensions.
-         * Initializes homogenous coordinate to 1, indicating a 3D vector
-         */
+        /// <summary>
+        /// Constructor that initializes only x, y, and z dimensions.
+        /// Initializes homogenous coordinate to 1, indicating a 3D vector
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
         public Vector4(double x, double y, double z)
         {
             v[0] = (float) x;
@@ -86,15 +100,27 @@ namespace SousChef
             v[3] = 1;
         }
 
-        /*
-         * Support for accessing individual coordinates of the vector
-         */
+        /// <summary>
+        /// Gets the specified dimension of the vector.
+        /// x = 0
+        /// y = 1
+        /// z = 2
+        /// w = 3
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
         public float this[int ind]
         {
             get {return v[ind];}
             set {v[ind] = value;}
         }
         
+        /// <summary>
+        /// Scalar multiplication.
+        /// </summary>
+        /// <param name="scalar"></param>
+        /// <param name="rhs"></param>
+        /// <returns>The scaled vector</returns>
         public static Vector4 operator * (float scalar, Vector4 rhs)
         {
 	        Vector4 retVect = new Vector4(rhs);
@@ -104,11 +130,23 @@ namespace SousChef
 	        return retVect;
         }
 
+        /// <summary>
+        /// Scalar multiplication.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="scalar"></param>
+        /// <returns>The scaled vector</returns>
         public static Vector4 operator * (Vector4 lhs, float scalar)
         {
             return scalar*lhs;
         }
 
+        /// <summary>
+        /// Vector multiplication.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>A scalar that represents the result of multiplying the two vectors</returns>
         public static float operator * (Vector4 lhs, Vector4 rhs)
         {
 	        return  lhs[0]*rhs[0] +
@@ -117,6 +155,12 @@ namespace SousChef
                     lhs[3]*rhs[3];
         }
 
+        /// <summary>
+        /// Vector addition
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>The Vector4 that is the result of the two vectors added together. This does NOT add the homogenous coordinate.</returns>
         public static Vector4 operator + (Vector4 lhs, Vector4 rhs)
         {
 	        return new Vector4( lhs[0]+rhs[0],
@@ -124,6 +168,12 @@ namespace SousChef
                                 lhs[2]+rhs[2]);	
         }
 
+        /// <summary>
+        /// Vector subtraction
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>The Vector4 that is the result of lhs-rhs. This does NOT add the homogenous coordinate.</returns>
         public static Vector4 operator - (Vector4 lhs, Vector4 rhs)
         {
 	        return new Vector4( lhs[0]-rhs[0],
@@ -131,6 +181,13 @@ namespace SousChef
 					            lhs[2]-rhs[2]);
         }
 
+        /// <summary>
+        /// Sets all dimensions of the vector.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="w"></param>
         public void Set(float x, float y, float z, float w)
         {
 	        v[0] = x;
@@ -139,6 +196,13 @@ namespace SousChef
 	        v[3] = w;
         }
         
+        /// <summary>
+        /// Sets all dimensions of the vector.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="w"></param>
         public void Set(double x, double y, double z, double w)
         {
 	        v[0] = (float) x;
@@ -147,6 +211,11 @@ namespace SousChef
 	        v[3] = (float) w;
         }
 
+        /// <summary>
+        /// Dots this vector and another vector, disregarding the homogenous coordinate.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>The dot product of this vector, disregarding the homogenous coordinate.</returns>
         public float DotProduct(Vector4 other)
         {
 	        return	v[0]*other[0]+
@@ -154,6 +223,11 @@ namespace SousChef
 			        v[2]*other[2];
         }
 
+        /// <summary>
+        /// Crosses this vector and another vector, disregarding the homogenous coordinate.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>The cross product of the vector, disregarding the homogenous coordinate.</returns>
         public Vector4 CrossProduct(Vector4 other)
         {
             float x = (v[1]*other[2]) - (v[2]*other[1]);
@@ -162,14 +236,30 @@ namespace SousChef
             return new Vector4(x,y,z,0);
         }
 
+        /// <summary>
+        /// Gets the magnitude of the 3D vector, disregarding the homogenous coordinate.
+        /// </summary>
+        /// <returns>The magnitude of the 3D vector, disregarding the homogenous coordinate.</returns>
+        public float Magnitude()
+        {
+            return (float)Math.Sqrt(v[0]*v[0]+
+				                    v[1]*v[1]+
+				                    v[2]*v[2]);
+        }
+
+        /// <summary>
+        /// Negates this vector, disregarding the homogeonous coordinate
+        /// </summary>
         public void Negate()
         {
 	        v[0]=-v[0];
 	        v[1]=-v[1];
 	        v[2]=-v[2];
-	        v[3]=-v[3];
         }
 
+        /// <summary>
+        /// Dehomogenizes the vector (divides every dimension of the vector by the homogenous coordinate)
+        /// </summary>
         public void Dehomogenize()
         {
 	        v[0] /= v[3];
@@ -177,33 +267,31 @@ namespace SousChef
 	        v[2] /= v[3];
 	        v[3] /= v[3];
         }
-
-
-        public float Magnitude()
-        {
-	        return (float) Math.Sqrt(   v[0]*v[0]+
-				                        v[1]*v[1]+
-				                        v[2]*v[2]);
-        }
-
-
+        /// <summary>
+        /// Normalizes the vector, skipping the homogenous coordinate.
+        /// </summary>
         public void Normalize()
         {
 	        float mag = this.Magnitude();
 	        v[0]=v[0]/mag;
 	        v[1]=v[1]/mag;
 	        v[2]=v[2]/mag;
-            v[3]=v[3]/mag;
         }
 
+        /// <summary>
+        /// Scales the vector, skipping the homogenous coordinate.
+        /// </summary>
+        /// <param name="scalar"></param>
         public void Scale(float scalar)
         {
 	        v[0] *= scalar;
 	        v[1] *= scalar;
 	        v[2] *= scalar;
-	        v[3] *= scalar;
         }
 
+        /// <summary>
+        /// Prints the vector to the console.
+        /// </summary>
         public void Print()
         {
 	        Console.Write(  "Vector4: " + "\n" + 
