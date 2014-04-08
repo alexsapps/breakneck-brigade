298086c525662a67a072faa75eb60835138661fa
Ryan <rageorge@ucsd.edu>
2014-04-08 12:57:03 -0700

    Added an empty class

---
+++ Breakneck Brigade/Breakneck Brigade/Graphics/ModelData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakneck_Brigade.Graphics
+{
+    class ModelData
+    {
+        public string TextureFilename;
+        public string ModelFilename;
+    }
+}
