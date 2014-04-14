c87e811a94673d812ee338bbdbbd6ec73aaf3cea
alexander taylor <ajxtaylor@gmail.com>
2014-04-14 07:57:27 -0700

    hashing for cooker and recipe

--- Breakneck Brigade/SousChef/Cooker.cs
+++ Breakneck Brigade/SousChef/Cooker.cs
@@ -9,13 +9,32 @@ namespace SousChef
     public class Cooker : GameObject
     {
         public CookerType Type { get; set; }
-        public List<Recipe> Contents { get; private set; }
+        public List<Ingredient> Contents { get; private set; }
 
         public Cooker (int id, CookerType type)
             : base(id)
         {
             this.Type = type;
-            Contents = new List<Recipe>();
+            Contents = new List<Ingredient>();
+        }
+
+        public Recipe Cook()
+        {
+            Contents.Sort();
+            
+            string hash = Recipe.Hash(Contents.ConvertAll<IngredientType>(x => x.Type));
+
+            foreach (var recipe in Type.Recipes)
+            {
+                if (recipe.hash() == hash)
+                {
+                    //TODO: clear contents and put the actual ingredient here (and still return the recipe for purpose of awarding points)
+                    //      current problem:  how to initialize new game object--need id getter, need to keep track of new objects
+                    return recipe;
+                }
+            }
+
+            return null;
         }
 
         public override void Update()
--- Breakneck Brigade/SousChef/Recipe.cs
+++ Breakneck Brigade/SousChef/Recipe.cs
@@ -8,13 +8,44 @@ namespace SousChef
 {
     public class Recipe 
     {
-        public List<IngredientType> Ingredients { get; private set; }
+        public string Name { get; private set; } //in case FinalProduct does not uniquely determine Recipe
+        public List<IngredientType> Ingredients { get; private set; } //if this ever changes, make sure to set _hash=null
         public IngredientType FinalProduct;
         
-        public Recipe(List<IngredientType> ingredients, IngredientType finalProduct)
+        public Recipe(string name, List<IngredientType> ingredients, IngredientType finalProduct)
         {
+            this.Name = name;
             this.Ingredients = ingredients;
             this.FinalProduct = finalProduct;
+
+            int sum = 0;
+            foreach (var ingredient in ingredients)
+                sum += ingredient.DefaultPoints;
+            if (finalProduct.DefaultPoints < sum)
+                throw new Exception("recipe " + Name + " must not be worth less than sum of it's parts");
+
+            hash(); //compute now
+        }
+
+        string _hash;
+        public string hash()
+        {
+            if(_hash == null)
+            {
+                _hash = Hash(Ingredients);
+            }
+            return _hash;
+        }
+
+        public static string Hash(List<IngredientType> list)
+        {
+            StringBuilder names = new StringBuilder();
+            foreach (IngredientType ingredient in list)
+            {
+                names.Append(ingredient.Name);
+                names.Append("|");
+            }
+            return names.ToString();
         }
     }
 }
