baf4d15462532ea77cdd0f42b35d4c9a1615dfdd
alexander taylor <ajxtaylor@gmail.com>
2014-04-26 00:18:16 -0700

    client box/plane, visual studio track my files!!

---
+++ Breakneck Brigade/Breakneck Brigade/ClientPlane.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakneck_Brigade
+{
+    class ClientPlane : ClientGameObject
+    {
+        public float Height { get; set; }
+        public string Texture { get; set; }
+        public override string ModelName
+        {
+            get { return Texture; }
+        }
+
+        public ClientPlane(int id, int height, string texture, ClientGame game) : base (id, game)
+        {
+            this.Height = height;
+            this.Texture = texture;
+        }
+
+        public ClientPlane(int id, BinaryReader reader, ClientGame game) : base (id, game, reader)
+        {
+            this.Height = reader.ReadSingle();
+            this.Texture = reader.ReadString();
+        }
+
+        protected override void readGeom(BinaryReader reader)
+        {
+            //don't read coordinates, do not call baes.readGeom()
+        }
+
+        public override void StreamUpdate(BinaryReader reader)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Breakneck Brigade/SousChef/Math/Coordinate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SousChef
+{
+    /// <summary>
+    /// simple, lightweight generic Coordinate class mainly for use by server
+    /// </summary>
+    public class Coordinate
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+        public Coordinate(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    public static partial class BB
+    {
+        public static void Write(this BinaryWriter stream, Coordinate value)
+        {
+            stream.Write((float)value.x);
+            stream.Write((float)value.y);
+            stream.Write((float)value.z);
+        }
+        //ReadCoordinate is in client program
+    }
+}
