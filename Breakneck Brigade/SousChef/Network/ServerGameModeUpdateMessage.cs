dc5233f7a9d2ac55cbf4b0503e0b9eb47a201717
alexsapps <ajxtaylor@gmail.com>
2014-05-06 17:36:56 -0700

    forgotten files in performance update

---
+++ Breakneck Brigade/SousChef/Network/ServerGameModeUpdateMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SousChef;
+using System.IO;
+
+namespace SousChef
+{
+    public class ServerGameModeUpdateMessage : ServerMessage
+    {
+        public GameMode Mode { get; set; }
+        public override ServerMessageType Type { get { return ServerMessageType.GameModeUpdate; } }
+        public override void Write(BinaryWriter writer)
+        {
+            writer.Write((byte)(Mode));
+        }
+
+        public ServerGameModeUpdateMessage() { }
+        public ServerGameModeUpdateMessage(BinaryReader reader)
+        {
+            Mode = (GameMode)reader.ReadByte();
+        }
+    }
+}
---
+++ Breakneck Brigade/SousChef/Network/ServerGameStateUpdateMessage.cs
@@ -0,0 +1,33 @@
+﻿using SousChef;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SousChef
+{
+    public class ServerGameStateUpdateMessage : ServerMessage
+    {
+        public override ServerMessageType Type { get { return ServerMessageType.GameStateUpdate; } }
+        public byte[] Binary;
+        
+        public override void Write(BinaryWriter writer)
+        {
+            BBStopwatch w1 = new BBStopwatch();
+            w1.Start();
+            writer.Write(Binary.Length);
+            writer.Write(Binary);
+            w1.Stop(2, "Client: slow game state write. {0}");
+        }
+
+        public ServerGameStateUpdateMessage() { }
+        public ServerGameStateUpdateMessage(BinaryReader reader)
+        {
+            int len = reader.ReadInt32();
+            Binary = reader.ReadBytes(len);
+        }
+    }
+}
